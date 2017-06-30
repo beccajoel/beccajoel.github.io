5c5c9de26603248f8501439e88cd769141149808
Rebecca <rkarol@usc.edu>
2017-06-29 20:25:46 -0400

    all the fun

--- tabscript.js
+++ tabscript.js
@@ -5,14 +5,14 @@ function openCity(evt, cityName) {
 
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {   
+    for (i = 1; i < tabcontent.length; i++) {
        tabcontent[i].style.display = "none";
     }
 
 
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
+    for (i = 1; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
